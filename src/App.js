f5d9f356c24aec1df5682d8ba7ec26dcc8e4a97a
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-07-18 18:22:11 +0200

    Started writing styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import "./App.css";
+import styled, { css } from "styled-components";
 
-import styled from "styled-components";
+// creating styles for the first component
+const HeaderOne = styled.h1`
+  color: red;
+  font-size: 80px;
+  text-align: center;
+  color: ${(props) => (props.color ? props.color : "red")};
+`;
+
+// creating styles for the second component
+const HeaderTwo = styled.h2`
+  ${(props) =>
+    props.primary &&
+    css`
+      color: tomato;
+      text-decoration: underline;
+      cursor: pointer;
+    `}
+`;
+
+// extending styles and overriding the font-size
+const HeaderThree = styled(HeaderOne)`
+  font-size: 65px;
+`;
 
 const App = () => {
-  return <div className="App">Hello there</div>;
+  return (
+    <div className="App">
+      <HeaderOne color={"green"}>Hello there</HeaderOne>
+      <HeaderTwo primary>This is just an example!</HeaderTwo>
+      <HeaderThree>This will extend the props of HeaderTwo</HeaderThree>
+    </div>
+  );
 };
 
 export default App;
